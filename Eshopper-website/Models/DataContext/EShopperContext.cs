8f96b559b7a3b81bc9278f404afc1b52641d097a
LN <luongquynhnhu0908@gmail.com>
2024-12-16 13:47:45 +0700

    Update
    Coupon

--- Eshopper-website/Models/DataContext/EShopperContext.cs
+++ Eshopper-website/Models/DataContext/EShopperContext.cs
@@ -13,5 +13,6 @@ namespace Eshopper_website.Models.DataContext
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
     }
 }
