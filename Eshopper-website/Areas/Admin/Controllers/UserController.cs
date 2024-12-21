93dd4adc888db7bf3c2df52420fa3302dc01d68c
LN <luongquynhnhu0908@gmail.com>
2024-12-21 21:09:18 +0700

    Update
    Login Page
    Account

---
+++ Eshopper-website/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using Eshopper_website.Areas.Admin.DTOs.request;
+using Eshopper_website.Models;
+using Eshopper_website.Models.DataContext;
+using Eshopper_website.Utils.Extension;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eshopper_website.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly EShopperContext _context;
+        public UserController(EShopperContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            // Check cookies
+            var login = Request.Cookies.Get<LoginDTO>("UserCredential");
+            if (login != null)
+            {
+                var result = _context.Accounts.AsNoTracking()
+                                .FirstOrDefault(x => x.ACC_Username == login.UserName &&
+                                    x.ACC_Password == login.Password);
+                if (result != null)
+                {
+                    // Set Session
+                    HttpContext.Session.Set<Account>("userInfo", result);
+                    // Redirect to Dashboard
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginDTO login)
+        {
+            var result = await _context.Accounts.AsNoTracking()
+                                .FirstOrDefaultAsync(x => x.ACC_Username == login.UserName &&
+                                    x.ACC_Password == login.Password);
+            if (result != null)
+            {
+                if (login.RememberMe)
+                {
+                    Response.Cookies.Append<LoginDTO>("UserCredential", login, new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddDays(7),
+                        HttpOnly = true, // Accessible only by the server
+                        IsEssential = true // Required for GDPR compliance
+                    });
+                }
+
+                // Set Session
+                HttpContext.Session.Set<Account>("userInfo", result);
+                // Redirect to Dashboard
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewData["Message"] = "Wrong username or password!";
+            }
+            return View();
+        }
+
+    }
+}
--- Eshopper-website/Models/DataContext/EShopperContext.cs
+++ Eshopper-website/Models/DataContext/EShopperContext.cs
@@ -1,5 +1,4 @@
-﻿using Eshopper_website.Models.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Eshopper_website.Models;
 
 namespace Eshopper_website.Models.DataContext
@@ -18,5 +17,6 @@ namespace Eshopper_website.Models.DataContext
         public DbSet<Blog>? Blogs { get; set; }
         public DbSet<Coupon>? Coupons { get; set; }
         public DbSet<Menu>? Menus { get; set; }
+        public DbSet<Account>? Accounts { get; set; }
     }
 }
